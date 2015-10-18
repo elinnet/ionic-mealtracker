ada2c025d5b02e00a6933d143a65d0c467faca81
Elin <elinnet@gmail.com>
2015-10-18 19:08:51 +0100

    meal tracker - validations placed on form - but contains errors.  Msg pop up too early, name+password msgs not correctly showing

---
+++ www/js/controllers/authentication.js
@@ -0,0 +1,41 @@
+var app = angular.module('mealtrack.controllers.authentication', []);
+
+/*********************************************************************
+ * LoginCtrl
+ *********************************************************************/
+app.controller('LoginCtrl', function ($scope, $state, AuthService) {
+
+	$scope.formData = {
+		"email": "",
+		"password": ""
+	};
+
+	$scope.login = function (form) {
+		console.log("LoginCtrl::login");
+		if (form.$valid){
+			//TODO
+		} else {
+		console.log("Invalid form");
+		}
+
+	};
+
+});
+
+/*********************************************************************
+ * SignupCtrl
+ *********************************************************************/
+app.controller('SignupCtrl', function ($scope, $state, AuthService) {
+
+	$scope.formData = {
+		"name": "",
+		"email": "",
+		"password": ""
+	};
+
+	$scope.signup = function () {
+		console.log("SignupCtrl::signup");
+		//TODO
+	};
+
+});
